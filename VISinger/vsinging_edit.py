34ee2cdd1ff4236882af6b17aa8f1434a34d2a4d
dtx525942103 <525942103@qq.com>
2022-03-31 07:12:50 +0000

    base finish

--- VISinger/vsinging_edit.py
+++ VISinger/vsinging_edit.py
@@ -22,14 +22,25 @@ net_g = SynthesizerTrn(
     hps.train.segment_size // hps.data.hop_length,
     **hps.model).cuda()
 
-_ = utils.load_checkpoint("./logs/singing_base/G_26000.pth", net_g, None)
+_ = utils.load_checkpoint("./logs/singing_base/G_270000.pth", net_g, None)
 net_g.eval()
 # net_g.remove_weight_norm()
 
+idx = "2044001628"
 text_norm = np.load(f"midis/singing_label.npy")
 text_tone = np.load(f"midis/singing_pitch.npy")
 input_ids = torch.LongTensor(text_norm)
 tune_ids = torch.LongTensor(text_tone)
+input_f0 = torch.load(f"../VISinger_data/wav_dump_16k/{idx}_bits16.f0.pt")
+len_text = input_ids.size()[0]
+len_tone = tune_ids.size()[0]
+len_spec = input_f0.size()[-1]
+assert len_text == len_tone
+if (len_text != len_spec):
+    len_min = min(len_text, len_spec)
+    input_ids = input_ids[:len_min]
+    tune_ids = tune_ids[:len_min]
+    input_f0 = input_f0[:len_min]
 
 begin_time = time()
 with torch.no_grad():
@@ -37,7 +48,8 @@ with torch.no_grad():
     x_tst_lengths = torch.LongTensor([input_ids.size(0)]).cuda()
     t_tst = tune_ids.cuda().unsqueeze(0)
     t_tst_lengths = torch.LongTensor([tune_ids.size(0)]).cuda()
-    audio = net_g.infer(x_tst, x_tst_lengths, t_tst, t_tst_lengths, noise_scale=0, noise_scale_w=0, length_scale=1)[0][0,0].data.cpu().float().numpy()
+    f0_tst = input_f0.cuda().unsqueeze(0)
+    audio = net_g.infer(x_tst, x_tst_lengths, t_tst, t_tst_lengths, f0_tst, t_tst_lengths, noise_scale=0, noise_scale_w=0, length_scale=1)[0][0,0].data.cpu().float().numpy()
 end_time = time()
 run_time = end_time - begin_time
 print('Syth Time (Seconds):', run_time)
--- VISinger/vsinging_infer.py
+++ VISinger/vsinging_infer.py
@@ -22,7 +22,7 @@ net_g = SynthesizerTrn(
     hps.train.segment_size // hps.data.hop_length,
     **hps.model).cuda()
 
-_ = utils.load_checkpoint("./logs/singing_base/G_26000.pth", net_g, None)
+_ = utils.load_checkpoint("./logs/singing_base/G_270000.pth", net_g, None)
 net_g.eval()
 # net_g.remove_weight_norm()
 
@@ -30,12 +30,22 @@ net_g.eval()
 if not os.path.exists("./singing_out"):
     os.makedirs("./singing_out")
 
-idxs = ["2001000001", "2001000002", "2001000003", "2001000004", "2001000005", "2001000006"]
+idxs = ["2001000001", "2001000002", "2001000003", "2001000004", "2001000005", "2001000006", "2051001912", "2051001913", "2051001914", "2051001915", "2051001916", "2051001917"]
 for idx in idxs:
     text_norm = np.load(f"../VISinger_data/label_vits/{idx}_label.npy")
     text_tone = np.load(f"../VISinger_data/label_vits/{idx}_pitch.npy")
+    input_f0 = torch.load(f"../VISinger_data/wav_dump_16k/{idx}_bits16.f0.pt")
     input_ids = torch.LongTensor(text_norm)
     tune_ids = torch.LongTensor(text_tone)
+    len_text = input_ids.size()[0]
+    len_tone = tune_ids.size()[0]
+    len_spec = input_f0.size()[-1]
+    assert len_text == len_tone
+    if (len_text != len_spec):
+        len_min = min(len_text, len_spec)
+        input_ids = input_ids[:len_min]
+        tune_ids = tune_ids[:len_min]
+        input_f0 = input_f0[:len_min]
 
     begin_time = time()
     with torch.no_grad():
@@ -43,7 +53,8 @@ for idx in idxs:
         x_tst_lengths = torch.LongTensor([input_ids.size(0)]).cuda()
         t_tst = tune_ids.cuda().unsqueeze(0)
         t_tst_lengths = torch.LongTensor([tune_ids.size(0)]).cuda()
-        audio = net_g.infer(x_tst, x_tst_lengths, t_tst, t_tst_lengths, noise_scale=0, noise_scale_w=0, length_scale=1)[0][0,0].data.cpu().float().numpy()
+        f0_tst = input_f0.cuda().unsqueeze(0)
+        audio = net_g.infer(x_tst, x_tst_lengths, t_tst, t_tst_lengths, f0_tst, t_tst_lengths, noise_scale=0, noise_scale_w=0, length_scale=1)[0][0,0].data.cpu().float().numpy()
     end_time = time()
     run_time = end_time - begin_time
     print('Syth Time (Seconds):', run_time)
